7fdca115a2c834dfd59f9ca58e266c1d0a1416f6
mindw96 <mindw96@naver.com>
2024-01-12 17:12:48 +0900

    fix: Rename Function sum2num to sumNums

--- adder.py
+++ adder.py
@@ -1,4 +1,4 @@
-def sum2num(num1, num2):
+def sumNums(num1, num2):
     return num1 + num2
 
 
@@ -6,4 +6,4 @@ if __name__=='__main__':
     num1 = int(input('첫번째 숫자를 입력하시오: '))
     num2 = int(input('두번째 숫자를 입력하시오: '))
     
-    print(sum2num(num1, num2))
+    print(sumNums(num1, num2))
