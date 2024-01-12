7b15b876c3724ff3f30c7d9f6841953d335fabe1
mindw96 <mindw96@naver.com>
2024-01-12 17:11:22 +0900

    feat: Add Feature
    Return sum of 2 numbers

---
+++ adder.py
@@ -0,0 +1,9 @@
+def sum2num(num1, num2):
+    return num1 + num2
+
+
+if __name__=='__main__':
+    num1 = int(input('첫번째 숫자를 입력하시오: '))
+    num2 = int(input('두번째 숫자를 입력하시오: '))
+    
+    print(sum2num(num1, num2))
